fb86fca59a438dcc4cecd105e10e7d6fcea749c6
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-05 21:51:16 -0600

    Update Program.cs

--- Libre-ERP-API/Program.cs
+++ Libre-ERP-API/Program.cs
@@ -1,3 +1,6 @@
+using Libre_ERP_API.Controllers;
+using Libre_ERP_API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ProductServices>();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapProductEndpoints();
 
 app.Run();
