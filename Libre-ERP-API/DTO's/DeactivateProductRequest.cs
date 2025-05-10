d513c15765adb0e79810ed439156a9516c2e0a17
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-09 22:28:18 -0600

    feat(product): add deactivate product dto

---
+++ Libre-ERP-API/DTO's/DeactivateProductRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace Libre_ERP_API.DTO_s
+{
+    public class DeactivateProductRequest
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid User")]
+        public int IDUser { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid Product")]
+        public int IDProduct { get; set; }
+    }
+}
