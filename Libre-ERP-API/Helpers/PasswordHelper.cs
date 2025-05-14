0af57bc1ea30c571f4012badf2096554f89c179e
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-13 18:11:25 -0600

    feat(helper): add password helper

---
+++ Libre-ERP-API/Helpers/PasswordHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Libre_ERP_API.Helpers
+{
+    public class PasswordHelper
+    {
+        private readonly string _secretKey;
+
+        public PasswordHelper(IConfiguration configuration)
+        {
+            _secretKey = configuration["PasswordSettings:SecretKey"]!;
+
+            if (string.IsNullOrEmpty(_secretKey))
+                throw new Exception("Secret key for password hashing is missing.");
+        }
+
+        public string HashPassword(string plainPassword)
+        {
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(_secretKey));
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(plainPassword));
+            return Convert.ToBase64String(hash);
+        }
+
+    }
+}
