0b18aea673b303fa686599914284b74a8a6e09f3
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-13 18:32:19 -0600

    feat(endpoint): add user controller & createUser endpoint

---
+++ Libre-ERP-API/Controllers/UserEndpoints.cs
@@ -0,0 +1,31 @@
+﻿using Libre_ERP_API.DTO_s;
+using Libre_ERP_API.DTO_s.User;
+using Libre_ERP_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libre_ERP_API.Controllers
+{
+    public static class UserEndpoints
+    {
+        public static void MapUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/user", async (CreateUserRequest dto, UserServices service) =>
+            {
+                try
+                {
+                    var result = await service.CreateUserAsync(dto);
+                    return Results.Ok(new BaseResponse
+                    {
+                        ErrorID = result.ErrorID,
+                        ErrorDescription = result.ErrorDescription
+                    });
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+            });
+        }
+    }
+}
