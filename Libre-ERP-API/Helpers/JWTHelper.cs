b1ce1b52e19561feb7273248f8434fc534154a14
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-16 21:21:35 -0600

    feat(helper): add a jwt generator

---
+++ Libre-ERP-API/Helpers/JWTHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Libre_ERP_API.Helpers
+{
+    public class JWTHelper
+    {
+        private readonly IConfiguration _config;
+
+        public JWTHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GenerateToken(int userId, string userName)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Name, userName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(10),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
