846450dec3a9760df16047c2b2372f8c530a3228
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-18 13:41:17 -0600

    feat(dto): add login req dto

---
+++ Libre-ERP-API/DTO's/User/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libre_ERP_API.DTO_s.User
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(150, ErrorMessage = "Email must be less than 150 characters.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+        [MaxLength(100, ErrorMessage = "Password must be less than 100 characters.")]
+        public required string Password { get; set; }
+
+    }
+}
