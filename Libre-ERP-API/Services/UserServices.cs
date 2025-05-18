79eae9227d44518c10e0e61d7b7c72a3287ad5cb
BrandonZS <brandon.zamora.salazar@outlook.com>
2025-05-18 13:39:22 -0600

    feat(user): add login service

--- Libre-ERP-API/Services/UserServices.cs
+++ Libre-ERP-API/Services/UserServices.cs
@@ -3,6 +3,7 @@ using Libre_ERP_API.DTO_s;
 using Libre_ERP_API.DTO_s.User;
 using Libre_ERP_API.Helpers;
 using Microsoft.Data.SqlClient;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Libre_ERP_API.Services
 {
@@ -37,5 +38,55 @@ namespace Libre_ERP_API.Services
 
             return await CommandHelpers.ExecuteNonQuery("SP_CREATE_USER", parameters);
         }
+
+
+        public async Task<LoginResponse> LoginAsync(LoginRequest req)
+        {
+            var hashedPassword = _passwordService.HashPassword(req.Password);
+            var parameters = new[]
+            {
+            new SqlParameter("@EMAIL", req.Email),
+            new SqlParameter("@PASSWORD", hashedPassword)
+            };
+
+            var (data, errorID, errorDescription) = await CommandHelpers.ExecuteReaderAsync<LoginUserDTO>(
+                "SP_LOGIN_USER",
+                parameters,
+                reader => new LoginUserDTO
+                {
+                    IdUser = reader.GetInt32(reader.GetOrdinal("ID_USER")),
+                    Name = reader.GetString(reader.GetOrdinal("NAME")),
+                    LangCode = reader.GetString(reader.GetOrdinal("LANG_CODE")),
+                    CurrencyCode = reader.GetString(reader.GetOrdinal("CURRENCY_CODE"))
+                }
+            );
+
+            if (errorID.HasValue || data.Count == 0)
+            {
+                return new LoginResponse
+                {
+                    Name = string.Empty,
+                    LangCode = string.Empty,
+                    CurrencyCode = string.Empty,
+                    Token = null,
+                    ErrorID = errorID ?? 1,
+                    ErrorDescription = errorDescription ?? "Credenciales inválidas o error desconocido."
+                };
+            }
+
+            var user = data[0];
+            var token = _jwtHelper.GenerateToken(user.IdUser, user.Name);
+
+            return new LoginResponse
+            {
+                Name = user.Name,
+                LangCode = user.LangCode,
+                CurrencyCode = user.CurrencyCode,
+                Token = token,
+                ErrorID = null,
+                ErrorDescription = null
+            };
+        }
+
     }
 }
