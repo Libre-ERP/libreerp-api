ead1f5f3a63f8fb172bee4a9fcf69d43a441e00d
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-05-21 18:42:24 -0600

    feat(api): add the time zone api

---
+++ Libre-ERP-API/Controllers/TimeZoneEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Libre_ERP_API.DTO_s;
+using Libre_ERP_API.Services;
+
+namespace Libre_ERP_API.Controllers
+{
+    public static class TimeZoneEndpoints
+    {
+        public static void MapTimeZoneEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/timezones", async (TimeZoneService service) =>
+            {
+                try
+                {
+                    var result = await service.GetTimeZonesAsync();
+                    return Results.Ok(result);
+                }catch (Exception ex)
+                {
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+                
+            });
+        }
+    }
+}
---
+++ Libre-ERP-API/Services/TimeZoneService.cs
@@ -0,0 +1,38 @@
+﻿using Libre_ERP_API.Data;
+using Libre_ERP_API.DTO_s;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Libre_ERP_API.Services
+{
+    public class TimeZoneService
+    {
+        public async Task<(List<GetTimeZonesResponse> Result, int? ErrorId, string? ErrorDescription )> GetTimeZonesAsync()
+        {
+
+            var outputParams = new[]
+{
+                new SqlParameter("@ERROR_ID", SqlDbType.Int) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ERROR_DESCRIPTION", SqlDbType.VarChar, -1) { Direction = ParameterDirection.Output }
+};
+
+            var result = await CommandHelpers.ExecuteReaderAsync<GetTimeZonesResponse>(
+                "SP_GET_TIME_ZONES",
+                outputParams,
+                reader => new GetTimeZonesResponse
+                {
+                    Id = reader.GetByte(0),
+                    Country = reader.GetString(1),
+                    TimeZone = reader.GetString(2)
+                });
+
+            var errorId = (int?)outputParams[0].Value;
+            var errorDescription = outputParams[1].Value?.ToString();
+
+            if (errorId != null)
+                return (new List<GetTimeZonesResponse>(), errorId, errorDescription);
+
+            return result;
+        }
+    }
+}
