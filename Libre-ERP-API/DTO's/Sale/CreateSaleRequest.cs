16b472e8ad0b16443c9066d464a1ce607bf28d7a
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-05-24 19:46:07 -0600

    feat(api): add the create sale api

---
+++ Libre-ERP-API/DTO's/Sale/CreateSaleRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Libre_ERP_API.DTO_s.Sale
+{
+    public class CreateSaleRequest
+    {
+        public int IdUser { get; set; }
+        public List<SaleDetailItem> SaleDetails { get; set; }
+    }
+    public class SaleDetailItem
+    {
+        public int IdProduct { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Libre-ERP-API/Services/SaleService.cs
@@ -0,0 +1,50 @@
+﻿using Libre_ERP_API.Data;
+using Libre_ERP_API.DTO_s;
+using Libre_ERP_API.DTO_s.Sale;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Libre_ERP_API.Services
+{
+    public class SaleService
+    {
+        public async Task<(int? ErrorID, string? ErrorDescription)> CreateSaleAsync(CreateSaleRequest req)
+        {
+
+            var saleDetailsTable = new DataTable();
+            saleDetailsTable.Columns.Add("ID_PRODUCT", typeof(int));
+            saleDetailsTable.Columns.Add("QUANTITY", typeof(int));
+
+            foreach (var item in req.SaleDetails)
+                saleDetailsTable.Rows.Add(item.IdProduct, item.Quantity);
+
+            var parameters = new[]
+            {
+                new SqlParameter("@ID_USER", req.IdUser),
+                new SqlParameter("@SALE_DETAILS", saleDetailsTable)
+                {
+                    SqlDbType = SqlDbType.Structured,
+                    TypeName = "dbo.SALE_DETAIL_TYPE"
+                },
+                new SqlParameter("@ERROR_ID", SqlDbType.Int) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ERROR_DESCRIPTION", SqlDbType.VarChar, -1) { Direction = ParameterDirection.Output }
+            };
+
+            await CommandHelpers.ExecuteNonQuery("SP_CREATE_SALE", parameters);
+
+            var errorId = (int?)parameters[2].Value;
+            var errorDesc = parameters[3].Value?.ToString();
+
+            if (errorId != null)
+            {
+                return (errorId, errorDesc);
+            }
+            else
+            {
+                return (errorId, "Sale created successfully");
+
+            }
+
+        }
+    }
+}
