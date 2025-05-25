46874282c9c9719080a879d8a59dc2f71b88de48
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-05-24 18:46:24 -0600

    feat(api): add the create transaction api

---
+++ Libre-ERP-API/Controllers/TransactionEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Libre_ERP_API.DTO_s.Transaction;
+using Libre_ERP_API.Services;
+
+namespace Libre_ERP_API.Controllers
+{
+    public static class TransactionEndpoint
+    {
+        public static void MapTransactionEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/transactions", async (CreateTransactionRequest req, TransactionService service) =>
+            {
+                var result = await service.CreateTransactionAsync(req);
+                return result;
+            });
+        }
+
+    }
+}
