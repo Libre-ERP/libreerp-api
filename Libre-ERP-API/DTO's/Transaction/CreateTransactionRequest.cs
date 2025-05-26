72a3dc542fa2170647f5abfa5369a4356185a599
Angel Cortes <145083737+AngelCortes2005@users.noreply.github.com>
2025-05-25 19:27:39 -0600

    feat(api): add the create transaction api (#36)

---
+++ Libre-ERP-API/DTO's/Transaction/CreateTransactionRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Libre_ERP_API.DTO_s.Transaction
+{
+    public class CreateTransactionRequest
+    {
+        public int IdUser { get; set; }
+        public string Type { get; set; } // "In" or "Ex"
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
