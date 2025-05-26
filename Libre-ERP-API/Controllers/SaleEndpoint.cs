9861052187eb6447fecf5c9788f8e58091d0d388
Angel Cortes <145083737+AngelCortes2005@users.noreply.github.com>
2025-05-25 19:28:08 -0600

    Erp 33 api insert a sale using sp create sale (#37)
    * Update README.md
    
    * feat(api): add the create sale api
    
    * feat(api): add the sale api(rm cache, .vs)
    
    ---------
    
    Co-authored-by: Brandon Zamora S. <brandon.zamora.salazar@outlook.com>

---
+++ Libre-ERP-API/Controllers/SaleEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Libre_ERP_API.DTO_s.Sale;
+using Libre_ERP_API.Services;
+using System.Runtime.CompilerServices;
+
+namespace Libre_ERP_API.Controllers
+{
+    public static class SaleEndpoint
+    {
+
+        public static async void MapSaleEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/sales", async (CreateSaleRequest req, SaleService service) =>
+            {
+                var result = await service.CreateSaleAsync(req);
+                return result;
+            });
+        }
+    }
+}
