805dbe6b96a86f06ed8821f5984158dc55cf78cf
Angel Cortes <145083737+AngelCortes2005@users.noreply.github.com>
2025-05-25 19:26:37 -0600

    feat(api): add the time zone api (#34)
    * feat(api): add the time zone api
    
    * feat(api): add the time zone api(rm cache)
    
    * feat(api): add the time zone api(rm .vs)
    
    * fix(api): changed id var type
    
    * fix(api): changed id var type reader
    
    * feat(api): add the create transaction api
    
    * Revert "feat(api): add the create transaction api"
    
    This reverts commit 9eec7cfeca3518a8c86d5c69d3f892e313ad0c8d.

---
+++ Libre-ERP-API/DTO's/TimeZone/GetTimeZonesResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Libre_ERP_API.DTO_s
+{
+    public class GetTimeZonesResponse : BaseResponse
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string TimeZone { get; set; }
+    }
+}
