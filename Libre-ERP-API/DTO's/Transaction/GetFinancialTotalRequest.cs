b0b1a7ca746f9bf32eb0a293854fe4a04f1536c5
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-06-04 20:48:20 -0600

    fix(api): add validation to req get financial total by date

--- Libre-ERP-API/DTO's/Transaction/GetFinancialTotalRequest.cs
+++ Libre-ERP-API/DTO's/Transaction/GetFinancialTotalRequest.cs
@@ -1,9 +1,28 @@
-﻿namespace Libre_ERP_API.DTO_s.Transaction
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libre_ERP_API.DTO_s.Transaction
 {
     public class GetFinancialTotalRequest
     {
+        [Required(ErrorMessage = "@ERROR_ID: IdUserRequired | @ERROR_DESCRIPTION: IdUser is required")]
         public int IdUser { get; set; }
+        [Required(ErrorMessage = "@ERROR_ID: StartDateRequired | @ERROR_DESCRIPTION: StartDate is required")]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "@ERROR_ID: EndDateRequired | @ERROR_DESCRIPTION: EndDate is required")]
         public DateTime EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            if (StartDate > EndDate)
+            {
+                errors.Add(new ValidationResult(
+                    "@ERROR_ID: InvalidDateRange | @ERROR_DESCRIPTION: StartDate must be earlier than or equal to EndDate",
+                    new[] { nameof(StartDate), nameof(EndDate) }));
+            }
+
+            return errors;
+        }
     }
 }
